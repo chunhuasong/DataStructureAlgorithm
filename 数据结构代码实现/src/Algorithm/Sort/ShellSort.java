999f5811a3439cd6762c9e6814f13565bce4cdca
chunhuasong <354293932@qq.com>
2021-11-02 14:26:17 +0800

    快排优化 希尔排序

--- 数据结构代码实现/src/Algorithm/Sort/ShellSort.java
+++ 数据结构代码实现/src/Algorithm/Sort/ShellSort.java
@@ -33,7 +33,7 @@ public class ShellSort {
 
     @Test
     public void test(){
-        int[] arr = {99, 5, 69, 33, 56, 13, 22, 55, 77, 48, 12, 88, 2,69,99};
+        int[] arr = {99, 5, 69, 33, 56, 13, 22, 55, 77, 48, 12, 88, 2,69};
         ShellSort(arr);
         System.out.println(Arrays.toString(arr));
 
